0ee01a749ff9f312487602033978dc440f9aa438
MrTWeeT <davidbaulenas@gmail.com>
2017-06-13 17:09:46 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,35 @@
+function pag_treballs(){ 
+window.location.href="index.html"; 
+}
+
+function pag_3d(){ 
+window.location.href="3d.html"; 
+}
+
+function pag_rig(){ 
+window.location.href="rig.html"; 
+}
+
+function pag_animacio(){ 
+window.location.href="animacio.html"; 
+}
+
+function pag_ilustracio(){ 
+window.location.href="ilustracio.html"; 
+}
+
+function pag_web(){ 
+window.location.href="web.html"; 
+}
+
+function pag_llum(){ 
+window.location.href="llum.html"; 
+}
+
+function pag_info(){ 
+window.location.href="info.html"; 
+}
+
+function pag_contact(){ 
+window.location.href="contact.html"; 
+}
